d85f3019afd557a9260d3aeb517ff072d42699bb
Rina Rim <rina.rim803@gmail.com>
2022-12-16 19:39:25 -0800

    Update structure editing todo items

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import style from "./App.module.css";
 import { useState } from "react";
 import uniqueId from "lodash.uniqueid";
-import EnteredItemList from "./component/EnteredItemList";
+import EnteredItem from "./component/EnteredItem";
 
 function App() {
   const [todoItem, setTodoItem] = useState("");
   const [todoList, setTodoList] = useState([]);
-
-  const [isChecked, setIsChecked] = useState(false);
-  const [editedTodo, setEditedTodo] = useState("");
-  const [editable, setEditable] = useState(false);
 
   function submitValueHandler(e) {
     e.preventDefault();
@@ -17,38 +13,28 @@ function App() {
     const prevUserTodoList = todoList;
     const todoId = uniqueId();
 
-    prevUserTodoList.push({ id: todoId, title: todoItem });
+    prevUserTodoList.push({ id: todoId, title: todoItem, editable: false });
     setTodoList(prevUserTodoList);
     setTodoItem("");
   }
 
-  function checkTodoHandler(e) {
-    setIsChecked(() => e.target.checked);
-    console.log(isChecked);
-  }
-
   function deleteTodoHandler(event, key) {
-    event.preventDefault();
-    if (isChecked) {
-      const prevUserTodoList = todoList;
-      setTodoList(() => {
-        return prevUserTodoList.filter((item) => item.id !== key);
-      });
-    }
-  }
-  function editTodoHandler(e) {
-    e.preventDefault();
-    const editedValue = e.target.value;
-    const key = e.target.id;
-    setEditedTodo(editedValue);
-
     const prevUserTodoList = todoList;
-    prevUserTodoList.map((item) => {
-      if (item.id === key) {
-        item.title = editedValue;
-      }
+    setTodoList(() => {
+      return prevUserTodoList.filter((item) => item.id !== key);
     });
+  }
+  function editTodoHandler(e, id) {
+    const prevUserTodoList = [...todoList];
+    const todo = prevUserTodoList.find((item) => item.id === id);
+    todo.title = e.target.value;
     setTodoList(prevUserTodoList);
+  }
+  function editableHandler(e, id) {
+    const newTodos = [...todoList];
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.editable = !todo.editable;
+    setTodoList(newTodos);
   }
 
   return (
@@ -64,28 +50,25 @@ function App() {
       </div>
 
       <div className={style}>
-        {todoList.map((item) => {
-          return (
-            <li key={item.id}>
-              <input type="checkbox" onChange={checkTodoHandler} />
-                {editable ? (
-                  <input
-                    type="text"
-                    id={item.id}
-                    value={item.title}
-                    onChange={(event) => editTodoHandler(event)}
-                  />
-                ) : (
-                  item.title
-                )}
-                <button onClick={()=>setEditable(!editable)}>Update</button>
-              <button onClick={(event) => deleteTodoHandler(event, item.id)}>
-                Delete
-              </button>
-            </li>
-          );
-        })}
-        {/* {todoList.length ? <EnteredItemList data={todoList} /> :"add todo lists"} */}
+        {todoList.length ? (
+          <ul>
+            {todoList.map((todoItem) => {
+              return (
+                <EnteredItem
+                  key={todoItem.id}
+                  id={todoItem.id}
+                  title={todoItem.title}
+                  editable={todoItem.editable}
+                  onClickTodo={editableHandler}
+                  deleteTodo={deleteTodoHandler}
+                  editTodo={editTodoHandler}
+                />
+              );
+            })}
+          </ul>
+        ) : (
+          "add todo lists"
+        )}
       </div>
     </div>
   );
--- src/component/EnteredItem.js
+++ src/component/EnteredItem.js
@@ -1,39 +1,20 @@
-import React, { useState } from "react";
-
 function EnteredItem(props) {
-  const [todoItem, setTodoItem] = useState("");
-
-  const [isChecked, setIsChecked] = useState(false);
-  const [editedTodo, setEditedTodo] = useState("");
-
-  function checkTodoHandler(e) {
-    setIsChecked(e.target.checked);
-  }
-
-  function editTodoHandler(editedValue, key) {
-    console.log(editedValue, key);
-    setEditedTodo(editedValue);
-    console.log(editedTodo);
-  }
-
-  function deleteTodoHandler(event, key) {}
-
   return (
     <div>
       <li key={props.id}>
-        <input type="checkbox" onChange={checkTodoHandler} />
-        <input
-          type="text"
-          id={props.id}
-          value={props.title}
-          onChange={(event) =>
-            editTodoHandler(event.target.value, event.target.id)
-          }
-        />
-        <button>Edit</button>
-        <button onClick={(event) => deleteTodoHandler(event, props.id)}>
-          Delete
-        </button>
+        <input type="checkbox" />
+        {props.editable ? (
+          <input
+            type="text"
+            id={props.id}
+            value={props.title}
+            onChange={(e) => props.editTodo(e, props.id)}
+          />
+        ) : (
+          props.title
+        )}
+        <button onClick={(e) => props.onClickTodo(e, props.id)}>Update</button>
+        <button onClick={(e) => props.deleteTodo(e, props.id)}>Delete</button>
       </li>
     </div>
   );
