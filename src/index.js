387ab54730f880cb2d42d5afb2054128a6addbcd
Rina Rim <rina.rim803@gmail.com>
2022-12-29 20:08:19 -0800

    Add Provider from react-redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-import React from "react";
-import { createStore } from "redux";
+import { combineReducers, createStore, applyMiddleware } from "redux";
 import ReactDOM from "react-dom/client";
+import logger from "redux-logger";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 
-const initialState = {
-  result: 1,
-  lastValues: [],
-  username: "Max"
-};
-const reducer = (state = initialState, action) => {
+const MathReducer = (state = { result: 1, lastValues: [] }, action) => {
   switch (action.type) {
     case "ADD":
       state = {
@@ -29,17 +25,50 @@ const reducer = (state = initialState, action) => {
   return state;
 };
 
-const store = createStore(reducer);
+const UserReducer = (state = { name: "Max", age: 27 }, action) => {
+  switch (action.type) {
+    case "SET_NAME":
+      state = {
+        ...state,
+        name: action.payload,
+      };
+      break;
+    case "SET_AGE":
+      state = {
+        ...state,
+        age: action.payload,
+      };
+      break;
+  }
+  return state;
+};
+const myLogger = (store) => (next) => (action) => {
+  //   console.log("Logged Action: ", action);
+  next(action);
+};
+const store = createStore(
+  combineReducers({ MathReducer, UserReducer }),
+  {},
+  applyMiddleware(logger)
+);
 
 store.subscribe(() => {
-  console.log("State changed", store.getState());
+  //   console.log("State changed", store.getState());
 });
 
 store.dispatch({
   type: "ADD",
   payload: 10,
 });
+store.dispatch({
+  type: "SET_AGE",
+  payload: 30,
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
